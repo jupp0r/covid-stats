3ed80b619042dc67a952606f48b8621e662c6870
jupp0r <jupp0r@users.noreply.github.com>
2020-05-09 22:23:14 +0000

    deploy: c170b161339ce6ce3d6ab96372e6b4b839fd1f3f

---
+++ precache-manifest.d48e36bb58f1a2430362770304304738.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ddd6dd22f1e538a8e0048bc956745b",
+    "url": "/covid-stats/index.html"
+  },
+  {
+    "revision": "28b48a9315da5058d778",
+    "url": "/covid-stats/static/css/main.cd06c4ff.chunk.css"
+  },
+  {
+    "revision": "c6821f5a0e18731e6284",
+    "url": "/covid-stats/static/js/2.930b06ca.chunk.js"
+  },
+  {
+    "revision": "3404d41fbe2e6348e200e5b3c7957ff3",
+    "url": "/covid-stats/static/js/2.930b06ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28b48a9315da5058d778",
+    "url": "/covid-stats/static/js/main.703e64ac.chunk.js"
+  },
+  {
+    "revision": "6fcca512ec5c646a08e3",
+    "url": "/covid-stats/static/js/runtime-main.e88c4e85.js"
+  }
+]);
