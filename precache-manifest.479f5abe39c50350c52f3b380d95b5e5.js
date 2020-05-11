b370b05860238c3bf1d4de7e41a8c9b855897b9f
jupp0r <jupp0r@users.noreply.github.com>
2020-05-11 22:41:46 +0000

    deploy: 2d8865cdab339207f64c50cf71269a2bab5084fa

---
+++ precache-manifest.479f5abe39c50350c52f3b380d95b5e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9e5d5173531deba6a26716f89b692c",
+    "url": "/covid-stats/index.html"
+  },
+  {
+    "revision": "55d2adf3f8836dff447a",
+    "url": "/covid-stats/static/css/2.a3860758.chunk.css"
+  },
+  {
+    "revision": "1b98b33f7efa6ee79d42",
+    "url": "/covid-stats/static/css/main.cd06c4ff.chunk.css"
+  },
+  {
+    "revision": "55d2adf3f8836dff447a",
+    "url": "/covid-stats/static/js/2.1c58772e.chunk.js"
+  },
+  {
+    "revision": "3404d41fbe2e6348e200e5b3c7957ff3",
+    "url": "/covid-stats/static/js/2.1c58772e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b98b33f7efa6ee79d42",
+    "url": "/covid-stats/static/js/main.841bee3f.chunk.js"
+  },
+  {
+    "revision": "6fcca512ec5c646a08e3",
+    "url": "/covid-stats/static/js/runtime-main.e88c4e85.js"
+  }
+]);
