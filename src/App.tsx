2d8865cdab339207f64c50cf71269a2bab5084fa
Jupp Mueller <jupp0r@gmail.com>
2020-05-11 15:40:18 -0700

    Add github ribbon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import "./App.css";
+import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
 import { State } from "./store";
 
@@ -13,19 +14,29 @@ import { assertNever } from "./utils";
 export const App = () => {
   const state: State = useSelector<State, State>(_ => _);
   return (
-    <div className="App">
-      {((state: State) => {
-        switch (state.type) {
-          case "loading":
-            return <Loading />;
-          case "loaded":
-            return <Loaded />;
-          case "error":
-            return <Error />;
-          default:
-            return assertNever(state);
-        }
-      })(state)}
-    </div>
+    <>
+      <a
+        className="github-fork-ribbon right-top"
+        href="https://github.com/jupp0r/covid-stats"
+        data-ribbon="Fork me on GitHub"
+        title="Fork me on GitHub"
+      >
+        Fork me on GitHub
+      </a>
+      <div className="App">
+        {((state: State) => {
+          switch (state.type) {
+            case "loading":
+              return <Loading />;
+            case "loaded":
+              return <Loaded />;
+            case "error":
+              return <Error />;
+            default:
+              return assertNever(state);
+          }
+        })(state)}
+      </div>
+    </>
   );
 };
