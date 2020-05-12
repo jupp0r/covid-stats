8124fd16ce9b7648857cca91bd50265541337743
Jupp Mueller <jupp0r@gmail.com>
2020-05-12 00:15:17 -0700

    Move data references to main page

--- src/Loaded.tsx
+++ src/Loaded.tsx
@@ -4,12 +4,21 @@ import { CaseChart } from "./CaseChart";
 import { CountryPicker } from "./CountryPicker";
 import { DeathCurve } from "./DeathCurve";
 
+const centered: React.CSSProperties = { textAlign: "center" };
+
 export const Loaded = () => {
   return (
     <div>
       <CountryPicker />
       <CaseChart />
       <DeathCurve />
+      <div style={centered}>
+        data from{" "}
+        <a href="https://covid.ourworldindata.org">Our World In Data</a> and{" "}
+        <a href="https://github.com/datasets/population">
+          datahub.io via the World Bank
+        </a>
+      </div>
     </div>
   );
 };
