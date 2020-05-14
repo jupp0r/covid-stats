56d40399127c84b02abf786b223394be67cfdab1
Jupp Mueller <jupp0r@gmail.com>
2020-05-13 17:30:20 -0700

    Refactor country picker selectors

--- src/CountryPicker.tsx
+++ src/CountryPicker.tsx
@@ -3,35 +3,46 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { LoadedState } from "./store";
 
+import { createSelector } from "reselect";
+
 import { includes } from "lodash/fp";
 import {
   makeCountryToggleAction,
   makeCoutrySearchChangedAction,
 } from "./actions";
+import { IDataFrame } from "data-forge";
 
-const coutrySelector = (state: LoadedState) =>
-  state.data
-    .select(
-      ({ iso_code, location }: { iso_code: string; location: string }) => ({
-        iso_code,
-        location,
-      }),
-    )
-    .distinct(row => row.iso_code)
-    .orderBy(row => row.location)
-    .toPairs()
-    .map(([_, row]) => ({
-      ...row,
-      active: includes(row.iso_code)(state.ui.pickedCountries),
-    }))
-    .filter(
-      row =>
-        row.active ||
-        (state.ui.searchText !== "" &&
-          row.location
-            .toLowerCase()
-            .startsWith(state.ui.searchText.toLowerCase())),
-    );
+const dataSelector = (state: LoadedState) => state.data;
+const pickedCountriesSelector = (state: LoadedState) =>
+  state.ui.pickedCountries;
+const searchTextSelector = (state: LoadedState) => state.ui.searchText;
+
+const coutrySelector = createSelector(
+  dataSelector,
+  pickedCountriesSelector,
+  searchTextSelector,
+  (data: IDataFrame, pickedCountries: string[], searchText) =>
+    data
+      .select(
+        ({ iso_code, location }: { iso_code: string; location: string }) => ({
+          iso_code,
+          location,
+        }),
+      )
+      .distinct(row => row.iso_code)
+      .orderBy(row => row.location)
+      .toPairs()
+      .map(([_, row]) => ({
+        ...row,
+        active: includes(row.iso_code)(pickedCountries),
+      }))
+      .filter(
+        row =>
+          row.active ||
+          (searchText !== "" &&
+            row.location.toLowerCase().startsWith(searchText.toLowerCase())),
+      ),
+);
 
 export const CountryPicker = () => {
   const dispatch = useDispatch();
