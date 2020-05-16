5b9df69f52993326feddca4b4087661daf32ff3a
jupp0r <jupp0r@users.noreply.github.com>
2020-05-16 06:37:50 +0000

    deploy: 69658c447dc4e7d2760951e181ea66caf1c36c67

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-stats/precache-manifest.ef7a5b1b57bb60530b75472d58202b46.js"
+  "/covid-stats/precache-manifest.33def46f9c6e30b0e3566a29f0eee20f.js"
 );
 
 self.addEventListener('message', (event) => {
